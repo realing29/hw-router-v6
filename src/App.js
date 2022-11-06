918379aea861674bcfff2beb2dd34bc70cbacd4b
realing29 <realing29@gmail.com>
2022-11-06 05:07:07 +0500

    ДЗ Роутер 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Navigate,
+  NavLink,
+  Outlet,
+  useNavigate,
+  useParams,
+  useRoutes,
+} from "react-router-dom";
 
-function App() {
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Main Page</h1>
+      <NavLink to="/users">Users list</NavLink>
     </div>
+  );
+};
+
+const UsersListPage = () => {
+  return (
+    <div>
+      <h1>Users Layout</h1>
+      <NavLink to="/">Home Page</NavLink>
+      <Outlet />
+    </div>
+  );
+};
+
+const UsersList = () => {
+  const [users] = useState(["q", "q", "q", "q"]);
+  return (
+    <ul>
+      {users.map((u, i) => (
+        <li key={"user" + i}>
+          <NavLink to={`/users/${i}/profile`}>User {i}</NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const UserPage = () => {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h1>User Page</h1>
+      <NavLink to={`/users/${userId}/edit`}>User edit</NavLink>
+      <br />
+      <NavLink to={`/users`}>Users list</NavLink>
+      <p>{"userId: " + userId}</p>
+    </div>
+  );
+};
+
+const UserEditPage = () => {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h1>User edit</h1>
+      <NavLink to={`/users/${userId}/profile`}>User page</NavLink>
+      <br />
+      <NavLink to={`/users/${+userId + 1}/profile`}>User another </NavLink>
+      <br />
+      <NavLink to={"/users/"}>Users list</NavLink>
+    </div>
+  );
+};
+
+const MyNavigate = () => {
+  const { userId } = useParams();
+  return <Navigate to={`/users/${userId}/profile`} />;
+};
+
+const routes = [
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "users",
+    element: <UsersListPage />,
+    children: [
+      { path: "", element: <UsersList /> },
+      {
+        path: ":userId",
+        children: [
+          { path: "", element: <MyNavigate /> },
+          { path: "profile", element: <UserPage /> },
+          { path: "edit", element: <UserEditPage /> },
+          { path: "*", element: <MyNavigate /> },
+        ],
+      },
+      { path: "*", element: <Navigate to="/users" /> },
+    ],
+  },
+];
+function App() {
+  const elements = useRoutes(routes);
+  return (
+    <>
+      <h1>App Layout</h1>
+      {elements}
+    </>
   );
 }
 
